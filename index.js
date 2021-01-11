cc5623b8cadb5bdd1b7ff435b7fc6e4774039675
urbanseabear <calvin.rudy@gmail.com>
2021-01-11 16:15:12 -0500

    update to files

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 
-const prefix = ';';
+const prefix = '^';
 
 client.on("message", function(message) {
   if (message.author.bot) { return;}
@@ -17,6 +17,10 @@ client.on("message", function(message) {
   if (command === 'ping') {
     const timeTaken = Date.now() - message.createdTimestamp;
     message.reply(`Ping! This message had a latency of ${timeTaken}ms. BEEP BOOP`);
+  } 
+  
+  else if (command === 'market') {
+
   }
 
   else if (command === 'random') {
@@ -28,6 +32,33 @@ client.on("message", function(message) {
       if (rando === 69) { message.reply('You rolled a 69. *nice*');}
       else { message.reply('You rolled: ' + rando); }
     }
+  }
+
+  else if (command === 'g') {
+    const randomPage = Math.floor(Math.random() * (470 - 2 + 1) ) + 2;
+    message.reply('Enjoy Jon\'s existential dread!\nhttps://garfieldminusgarfield.net/page/' + randomPage);
+  }
+
+  else if (command === 'pennyspeak') {
+    const opening = ['poggers, i', 'just', '', '', '', '', 'ask me how i', 'completely', 'poggers', 'Tachi', 'nearly', 'productively', 'efficiently', 'last night i', 'poggers', 'that wizard', 'a ninja', 'a seedy old man','Doc'];
+    const verbs = ['███████', 'downloaded', 'interfaced', 'deployed', 'developed', 'built', 'invented', 'experienced', 'navigated', 'hax0red', 'aided', 'enjoyed', 'engineered', 'farted', 'installed', 'debugged', 'delegated', 'automated', 'formulated', 'systematized', 'overhauled', 'computed'];
+    const objects = ['my', 'your', 'the', 'a', 'my', 'an entire', 'this', 'that', 'the', 'the big', 'a new form of', 'some type of', 'a whole lotta'];
+    const nouns = ['cat', 'tachi', 'your face', 'koolaid', 'system', 'city', 'butts', 'worm', 'cloud', 'potato', 'money', 'way of life', 'uwu', 'belief system', 'security system', 'gamer', 'bad decision', 'future', 'life', 'pony', 'mind'];
+    const tags = ['#techlife', '#pog', '#sf', '#biggestyike', 'but only i know how', 'for real', '#UwU', '#ffxiv', '#omg', 'OwO', '#yolo', '#magic', '', '', '', ''];
+
+    const randomElement = function(array){
+      const randomIndex = Math.floor(Math.random() * array.length);
+      return array[randomIndex];
+    };
+
+    const randomMessage = function(){
+      return [randomElement(opening), randomElement(verbs), randomElement(objects), randomElement(nouns), randomElement(tags)].join(' ');
+    };
+    message.reply(randomMessage());
+  }
+
+  else {
+    message.reply("-_- INVALID REQUEST -_-");
   }
 });
 
