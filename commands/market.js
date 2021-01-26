d876b768909acfda193a58f2ce0706d6b4e315fa
urbanseabear <calvin.rudy@gmail.com>
2021-01-25 20:01:01 -0500

    market command changes

--- commands/market.js
+++ commands/market.js
@@ -19,19 +19,25 @@ module.exports = {
         .then((itemObj) => {
           axios.get( `https://universalis.app/api/Crystal/${itemObj.ID}`)
           .then((itemInfo) => {
-            let itemList = [];
+            let pricePer = [];
+            let worlds = [];
+            let total = [];
             let listings = itemInfo.data.listings;
             listings.sort((a,b) => a.pricePerUnit - b.pricePerUnit);
             for (let i = 0; i < listings.length; i++) {
-              listing = `World: ${listings[i].worldName} Price(each):  ${listings[i].pricePerUnit} Total:  ${listings[i].total}\n`;
-              itemList.push(listing);
+              worlds.push(`${listings[i].worldName}`);
+              pricePer.push(`${listings[i].pricePerUnit} x ${listings[i].total / listings[i].pricePerUnit}`);
+              total.push(`${listings[i].total}`);
             }
-            let pages = Math.floor(itemList.length / 10);
-            let finalpage = itemList.length % 10;
+            let pages = Math.floor(total.length / 10);
+            let finalpage = total.length % 10;
             let embed = new Discord.MessageEmbed()
-          .setImage(`https://i.ibb.co/QdNxT3m/Gil-Icon.png`)
+          .setAuthor(`Lowest Price is on:  ${worlds[0]}`, `https://i.ibb.co/QdNxT3m/Gil-Icon.png`)
           .setThumbnail(`https://xivapi.com${itemObj.Icon}`)
-          .setDescription(itemList.slice(0, 10).join('\n'));
+          .setDescription(`${itemObj.Name}`)
+          .addField('World', worlds.slice(0, 10).join('\n'), true)
+          .addField(`Price Per x Quantity`, pricePer.slice(0, 10).join('\n'), true)
+          .addField(`Total`, total.slice(0, 10).join('\n'), true);
           message.reply(`Showing 10 cheapest ${itemObj.Name} on Crystal`);
           message.channel.send(embed);
           })
